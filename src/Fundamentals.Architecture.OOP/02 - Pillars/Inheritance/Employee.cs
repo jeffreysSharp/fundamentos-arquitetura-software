d506a822abc26fe41ce4be546d9b4117f2c39e1b
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-03-13 15:09:28 -0300

    Inmplementando Herança

---
+++ src/Fundamentals.Architecture.OOP/02 - Pillars/Inheritance/Employee.cs
@@ -0,0 +1,14 @@
+﻿// Classe derivada
+namespace Fundamentals.Architecture.OOP.Pillars.Inheritance
+{
+    public class Employee : Person
+    {
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} está trabalhando como {Position}.");
+        }
+    }
+}
--- src/Fundamentals.Architecture.OOP/Program.cs
+++ src/Fundamentals.Architecture.OOP/Program.cs
@@ -1,12 +1,33 @@
-﻿using Fundamentals.Architecture.OOP.ClassVsObjects;
-
-// Criando um objeto da classe House
-House myHouse = new House("Azul", 3, 120.5);
-
-// Exibindo informações
-myHouse.DisplayInfo();
+﻿/////////////////////////////////////////////////////
+///Herança
+using Fundamentals.Architecture.OOP.Pillars.Inheritance;
 
 
-// Abrindo e fechando
-myHouse.OpenDoor();
-myHouse.CloseDoor();
+var employee = new Employee
+{
+    Name = "Jeferson Almeida",
+    Age = 43,
+    Position = "Desenvolvedor Full Stack",
+    Salary = 10000,
+};
+
+employee.Introduce();
+employee.Work();
+
+
+
+
+/////////////////////////////////////////////////////
+///Estado e comportamento
+//using Fundamentals.Architecture.OOP.ClassVsObjects;
+
+//// Criando um objeto da classe House
+//House myHouse = new House("Azul", 3, 120.5);
+
+//// Exibindo informações
+//myHouse.DisplayInfo();
+
+
+//// Abrindo e fechando
+//myHouse.OpenDoor();
+//myHouse.CloseDoor();
